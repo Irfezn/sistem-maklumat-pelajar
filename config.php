fd900c586da8331f60e5bfb52c26fe37887d42ac
Cloudyky <ikhwanch131@gmail.com>
2025-11-04 12:40:20 +0800

    origin main

--- config.php
+++ config.php
@@ -1,3 +1,16 @@
 <?php
 
-    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "maklumat-pelajar";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully (MySQLi)!";
+
+?>
