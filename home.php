f7e8b219f8c81d49a38a78b0c70e500881be0d13
Irfezn <irfanbhl07@gmail.com>
2025-11-04 12:59:49 +0800

    baru

--- home.php
+++ home.php
@@ -7,14 +7,11 @@
     <link rel="stylesheet" href="">
 </head>
 <body>
-    <div class="header">
-        <img src="" alt="Logo">
-    </div>
-    <div class="content">
+    <div class="content" align="center">
         <h1>Selamat Datang Ke Sistem Maklumat Pelajar SKM</h1>
-        <button class="login-btn"><a href="lgpg.php">Kohort 2022</a></button>
-        <button class="login-btn"><a href="lgpg.php">Kohort 2023</a></button>
-        <button class="login-btn"><a href="lgpg.php">Kohort 2024</a></button>
+        <button><a href="">Kohort 2022</a></button>
+        <button><a href="">Kohort 2023</a></button>
+        <button><a href="">Kohort 2024</a></button>
     </div>
 </body>
 </html>
