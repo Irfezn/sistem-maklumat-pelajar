9e745ecf6bf295b5ddea1ee30ae46fa2d283d2cf
Irfezn <irfanbhl07@gmail.com>
2025-11-04 12:43:55 +0800

    nah

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <div class="header">
+        <img src="" alt="Logo">
+    </div>
+    <div class="content">
+        <h1>Selamat Datang Ke Sistem Maklumat Pelajar SKM</h1>
+        <button class="login-btn"><a href="lgpg.php">Kohort 2022</a></button>
+        <button class="login-btn"><a href="lgpg.php">Kohort 2023</a></button>
+        <button class="login-btn"><a href="lgpg.php">Kohort 2024</a></button>
+    </div>
+</body>
+</html>
