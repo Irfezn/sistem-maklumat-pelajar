c9b06b53c85e34e1ec49651b89e2c8fb12c68017
Irfezn <irfanbhl07@gmail.com>
2025-11-09 22:37:43 +0800

    css baru

--- home.php
+++ home.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ms">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
-    <link rel="stylesheet" href="">
+    <title>Laman Utama</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="content" align="center">
         <h1>Selamat Datang Ke Sistem Maklumat Pelajar SKM</h1>
-        <p>Sila pilih kohort yang berkenaann</p>
-        <button><a href="">Kohort 2022</a></button>
-        <button><a href="">Kohort 2023</a></button>
-        <button><a href="">Kohort 2024</a></button>
+        <p>Sila pilih kohort yang berkenaan</p>
+        <div class="button-group">
+            <a href="#" class="btn">Kohort 2022</a>
+            <a href="#" class="btn">Kohort 2023</a>
+            <a href="#" class="btn">Kohort 2024</a>
+        </div>
     </div>
 </body>
 </html>
